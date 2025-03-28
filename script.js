2a52a279fd5dd2c98006b3601285e99608959f7e
ManimehalaiAnandaraj <manimehalaiannakodi@gmail.com>
2025-03-28 12:00:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 //your JS code here. If required.
+let currentAudio;
+function playSound(sound) {
+	if(currentAudio){
+		stopSound();
+	}
+	currentAudio=new Audio(`sound/${sound}.mp3`);
+	currentAudio.play();
+}
+
+function stopSound() {
+	if(currentAudio){
+		currentAudio.pause();
+		currentAudio.currentTime=0;
+		currentAudio=null;
+	}
+	
+}
