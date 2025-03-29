30797235f7561d10c069442e6bc4bc871e08412b
ManimehalaiAnandaraj <manimehalaiannakodi@gmail.com>
2025-03-29 12:16:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,21 +1,19 @@
 //your JS code here. If required.
- const sounds={
+const sounds={
 	'applause':'baralho-101467.mp3',
-	 'boo':'aduio-1-20577.mp3',
-		'gasp':'baralho-101467.mp3',
+	'boo':'aduio-1-20577.mp3',
+	'gasp':'baralho-101467.mp3',
 	'tada': 'aduio-1-20577.mp3',
 	'victory':'baralho-101467.mp3',
-	 'wrong': 'baralho-101467.mp3',
-	 
+	'wrong': 'baralho-101467.mp3',
+};
 
- };
-	 
 let currentAudio;
 function PlayingAudio(sound) {
 	if(currentAudio){
 		stopSound();
 	}
-	currentAudio=new Audio(`sound/${sounds[sound]});
+	currentAudio=new Audio(`sounds/${sounds[sound]}`);
 	currentAudio.play();
 }
 
@@ -25,6 +23,4 @@ function stopSound() {
 		currentAudio.currentTime=0;
 		currentAudio=null;
 	}
-	}
-	
 }
